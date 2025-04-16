19037e8ad34e41beacc9cf26125a0c4a9ebd0c64
Calebe <calebepfinger@gmail.com>
2025-04-16 20:05:26 -0300

    Inserindo a funcionalidade principal, mover o robô e exibindo os resultados

--- RoboTupiniquimConsoleApp/Program.cs
+++ RoboTupiniquimConsoleApp/Program.cs
@@ -4,6 +4,10 @@
     {
         static void Main(string[] args)
         {
+            int posicaoX = 0;
+            int posicaoY = 0;
+            int apontando = 1;
+
             Console.WriteLine("----------------------------------------------------");
             Console.WriteLine("Robô Tupiniquim 2025");
             Console.WriteLine("----------------------------------------------------");
@@ -14,18 +18,76 @@
 
             string comandos = Console.ReadLine();
 
-            char [] matrizComandos = new char [comandos.Length];
+            string [] matrizComandos = new string [comandos.Length];
 
             for (int i = 0; i < comandos.Length; i++)
             {
-                matrizComandos [i] = comandos[i];
+                matrizComandos[i] = Convert.ToString(comandos[i]);
             }
 
             for (int i = 0; i < comandos.Length; i++)
             {
-                Console.WriteLine(matrizComandos[i]);
+                if (matrizComandos[i].ToUpper() == "M")
+                {
+                    if (apontando == 1)
+                    {
+                        posicaoY++;
+                    }
+
+                    else if (apontando == 2)
+                    {
+                        posicaoY--;
+                    }
+
+                    else if (apontando == 3)
+                    {
+                        posicaoX++;
+                    }
+
+                    else if (apontando == 4)
+                    {
+                        posicaoX--;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Temos um problema na variável apontando...");
+                    }
+                }
+
+                else if (matrizComandos[i].ToUpper() == "D")
+                {
+                    apontando++;
+                    if (apontando == 5)
+                    {
+                        apontando = 1;
+                    }
+                }
+
+                else if (matrizComandos[i].ToUpper() == "E")
+                {
+                    apontando--;
+                    if (apontando == 0)
+                    {
+                        apontando = 4;
+                    }
+                }
             }
-            Console.ReadLine();
+
+            string stgApontando = null;
+            string [] pontosCardiais = {null, "norte", "leste", "sul", "oeste"};
+
+            for (int i = 1; i < 5; i++)
+            {
+                if (apontando == i)
+                {
+                    stgApontando = pontosCardiais[i];
+                }
+            }
+
+            Console.WriteLine($"O robô está na localização {posicaoX}:{posicaoY}, virado para o {stgApontando}");
+    
+                Console.ReadLine();
         }
     }
 }
