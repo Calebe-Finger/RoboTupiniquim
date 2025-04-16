b8d232a2a0bdecab9fd26c99803dcfa4eb4dea95
Calebe <calebepfinger@gmail.com>
2025-04-16 15:07:37 -0300

    Quebra da String inserida pelo usuário em uma matriz char

--- RoboTupiniquimConsoleApp/Program.cs
+++ RoboTupiniquimConsoleApp/Program.cs
@@ -11,6 +11,21 @@
             Console.WriteLine("----------------------------------------------------");
             Console.WriteLine("Insira os comandos que serão executados: ");
             Console.WriteLine("----------------------------------------------------");
+
+            string comandos = Console.ReadLine();
+
+            char [] matrizComandos = new char [comandos.Length];
+
+            for (int i = 0; i < comandos.Length; i++)
+            {
+                matrizComandos [i] = comandos[i];
+            }
+
+            for (int i = 0; i < comandos.Length; i++)
+            {
+                Console.WriteLine(matrizComandos[i]);
+            }
+            Console.ReadLine();
         }
     }
 }
