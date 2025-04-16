eeb2fa3c6d46934a36ab0ed070847d5729dc2225
Calebe <calebepfinger@gmail.com>
2025-04-15 21:15:45 -0300

    Adicionar arquivos de projeto.

---
+++ RoboTupiniquimConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RoboTupiniquimConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
