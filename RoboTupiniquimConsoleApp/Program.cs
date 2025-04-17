545208730804324c3bac2a59273c3f9cafab4cab
Calebe <calebepfinger@gmail.com>
2025-04-17 15:17:37 -0300

    Validação dos valores inseridos pelo usuário

--- RoboTupiniquimConsoleApp/Program.cs
+++ RoboTupiniquimConsoleApp/Program.cs
@@ -6,6 +6,8 @@
         {
             while (true)
             {  
+                Console.Clear();
+
                 int posicaoX = 0;
                 int posicaoY = 0;
                 int apontando = 1;
@@ -25,18 +27,45 @@
                 if (coordenadaInicial == "S")
                     break;
 
-                else if (coordenadaInicial.Contains('M') == false && coordenadaInicial.Contains('E') == false && coordenadaInicial.Contains('D') == false) ;
+                else if (coordenadaInicial.Length > 2 || coordenadaInicial.Length == 1)
+                {
+                    Console.WriteLine("Por favor insira apenas dois numeros referentes a coordenada inicial: ");
+                    continue;
+                }
 
-                posicaoX = (Convert.ToInt32(coordenadaInicial[0])) - 48;
-                posicaoY = (Convert.ToInt32(coordenadaInicial[1])) - 48;
+                else if (coordenadaInicial == "")
+                    Console.WriteLine("Os valores iniciais serão definidos como 0:0");
+
+                else
+                {
+                    posicaoX = (Convert.ToInt32(coordenadaInicial[0])) - 48;
+                    posicaoY = (Convert.ToInt32(coordenadaInicial[1])) - 48;
+                }
 
                 Console.WriteLine("----------------------------------------------------");
                 Console.WriteLine("Para onde ele está virado: ");
+                Console.WriteLine("N - norte");
+                Console.WriteLine("S - sul");
+                Console.WriteLine("L - leste");
+                Console.WriteLine("O - oeste");
                 Console.WriteLine("----------------------------------------------------");
 
                 coordenadaInicial = Console.ReadLine();
+                coordenadaInicial = coordenadaInicial.ToUpper();
 
-                if (coordenadaInicial == "N")
+                if (coordenadaInicial == "")
+                {
+                    Console.WriteLine("O sentido inicial será definido como norte (N)");
+                    coordenadaInicial = "N";
+                }
+
+                else if (coordenadaInicial.Length > 1)
+                {
+                    Console.WriteLine("Por favor insira apenas uma letra referente ao sentido inicial: ");
+                    continue;
+                }
+
+                else if (coordenadaInicial == "N")
                 {
                     apontando = 1;
                 }
@@ -56,11 +85,32 @@
                     apontando = 4;
                 }
 
+                else
+                {
+                    Console.WriteLine("Por favor, insira um sentido válido: 'N', 'S', 'L' ou 'O'");
+                }
+
                 Console.WriteLine("----------------------------------------------------");
                 Console.WriteLine("Insira os comandos que serão executados: ");
+                Console.WriteLine("M - mover");
+                Console.WriteLine("E - esquerda");
+                Console.WriteLine("D - direita");
                 Console.WriteLine("----------------------------------------------------");
 
                 string comandos = Console.ReadLine();
+                comandos = comandos.ToUpper();
+
+                if (comandos == "")
+                {
+                    Console.WriteLine("Por favor, insira letras válidas: 'M', 'E' e 'D'");
+                    continue;
+                }
+                else if (comandos.Contains('M') == false && comandos.Contains('E') == false
+                    && comandos.Contains('D') == false)
+                {
+                    Console.WriteLine("Apenas são aceitos como comandos as letras 'M', 'E' e 'D'");
+                    continue;
+                }
 
                 string[] matrizComandos = new string[comandos.Length];
 
