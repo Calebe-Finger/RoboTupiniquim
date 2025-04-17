a42f3fd7ec28d22017cca6d42b39390130b8710f
Calebe <calebepfinger@gmail.com>
2025-04-17 14:18:16 -0300

    Inserindo um loop infinito no código para possibilitar o código rodas várias vezes seguidas. E a opção para sair do loop, inserindo o valor S

--- RoboTupiniquimConsoleApp/Program.cs
+++ RoboTupiniquimConsoleApp/Program.cs
@@ -4,124 +4,135 @@
     {
         static void Main(string[] args)
         {
-            int posicaoX = 0;
-            int posicaoY = 0;
-            int apontando = 1;
+            while (true)
+            {  
+                int posicaoX = 0;
+                int posicaoY = 0;
+                int apontando = 1;
 
-            Console.WriteLine("----------------------------------------------------");
-            Console.WriteLine("Robô Tupiniquim 2025");
-            Console.WriteLine("----------------------------------------------------");
+                Console.WriteLine("----------------------------------------------------");
+                Console.WriteLine("Robô Tupiniquim 2025");
+                Console.WriteLine("----------------------------------------------------");
 
-            Console.WriteLine("----------------------------------------------------");
-            Console.WriteLine("Qual é a posição atual do robô: ");
-            Console.WriteLine("----------------------------------------------------");
+                Console.WriteLine("----------------------------------------------------");
+                Console.WriteLine("Qual é a posição atual do robô: ");
+                Console.WriteLine("S - Para Sair: ");
+                Console.WriteLine("----------------------------------------------------");
 
-            string coordenadaInicial = Console.ReadLine();
+                string coordenadaInicial = Console.ReadLine();
+                coordenadaInicial = coordenadaInicial.ToUpper();
 
-            posicaoX = (Convert.ToInt32(coordenadaInicial[0])) - 48;
-            posicaoY = (Convert.ToInt32(coordenadaInicial[1])) - 48;
+                if (coordenadaInicial == "S")
+                    break;
 
-            Console.WriteLine("----------------------------------------------------");
-            Console.WriteLine("Para onde ele está virado: ");
-            Console.WriteLine("----------------------------------------------------");
+                else if (coordenadaInicial.Contains('M') == false && coordenadaInicial.Contains('E') == false && coordenadaInicial.Contains('D') == false) ;
 
-            coordenadaInicial = Console.ReadLine();
-            
-            if (coordenadaInicial.ToUpper() == "N")
-            {
-                apontando = 1;
-            }
+                posicaoX = (Convert.ToInt32(coordenadaInicial[0])) - 48;
+                posicaoY = (Convert.ToInt32(coordenadaInicial[1])) - 48;
 
-            else if (coordenadaInicial.ToUpper() == "L")
-            {
-                apontando = 2;
-            }
+                Console.WriteLine("----------------------------------------------------");
+                Console.WriteLine("Para onde ele está virado: ");
+                Console.WriteLine("----------------------------------------------------");
 
-            else if (coordenadaInicial.ToUpper() == "S")
-            {
-                apontando = 3;
-            }
+                coordenadaInicial = Console.ReadLine();
 
-            else if (coordenadaInicial.ToUpper() == "O")
-            {
-                apontando = 4;
-            }
-
-            Console.WriteLine("----------------------------------------------------");
-            Console.WriteLine("Insira os comandos que serão executados: ");
-            Console.WriteLine("----------------------------------------------------");
-
-            string comandos = Console.ReadLine();
-
-            string [] matrizComandos = new string [comandos.Length];
-
-            for (int i = 0; i < comandos.Length; i++)
-            {
-                matrizComandos[i] = Convert.ToString(comandos[i]);
-            }
-
-            for (int i = 0; i < comandos.Length; i++)
-            {
-                if (matrizComandos[i].ToUpper() == "M")
+                if (coordenadaInicial == "N")
                 {
-                    if (apontando == 1)
+                    apontando = 1;
+                }
+
+                else if (coordenadaInicial == "L")
+                {
+                    apontando = 2;
+                }
+
+                else if (coordenadaInicial == "S")
+                {
+                    apontando = 3;
+                }
+
+                else if (coordenadaInicial == "O")
+                {
+                    apontando = 4;
+                }
+
+                Console.WriteLine("----------------------------------------------------");
+                Console.WriteLine("Insira os comandos que serão executados: ");
+                Console.WriteLine("----------------------------------------------------");
+
+                string comandos = Console.ReadLine();
+
+                string[] matrizComandos = new string[comandos.Length];
+
+                for (int i = 0; i < comandos.Length; i++)
+                {
+                    matrizComandos[i] = Convert.ToString(comandos[i]);
+                }
+
+                for (int i = 0; i < comandos.Length; i++)
+                {
+                    if (matrizComandos[i] == "M")
                     {
-                        posicaoY++;
+                        if (apontando == 1)
+                        {
+                            posicaoY++;
+                        }
+
+                        else if (apontando == 2)
+                        {
+                            posicaoY--;
+                        }
+
+                        else if (apontando == 3)
+                        {
+                            posicaoX++;
+                        }
+
+                        else if (apontando == 4)
+                        {
+                            posicaoX--;
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Temos um problema na variável apontando...");
+                        }
                     }
 
-                    else if (apontando == 2)
+                    else if (matrizComandos[i] == "D")
                     {
-                        posicaoY--;
+                        apontando++;
+                        if (apontando == 5)
+                        {
+                            apontando = 1;
+                        }
                     }
 
-                    else if (apontando == 3)
+                    else if (matrizComandos[i] == "E")
                     {
-                        posicaoX++;
-                    }
-
-                    else if (apontando == 4)
-                    {
-                        posicaoX--;
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("Temos um problema na variável apontando...");
+                        apontando--;
+                        if (apontando == 0)
+                        {
+                            apontando = 4;
+                        }
                     }
                 }
 
-                else if (matrizComandos[i].ToUpper() == "D")
+                string stgApontando = null;
+                string[] pontosCardiais = { null, "norte", "leste", "sul", "oeste" };
+
+                for (int i = 1; i < 5; i++)
                 {
-                    apontando++;
-                    if (apontando == 5)
+                    if (apontando == i)
                     {
-                        apontando = 1;
+                        stgApontando = pontosCardiais[i];
                     }
                 }
 
-                else if (matrizComandos[i].ToUpper() == "E")
-                {
-                    apontando--;
-                    if (apontando == 0)
-                    {
-                        apontando = 4;
-                    }
-                }
+                Console.WriteLine($"O robô está na localização {posicaoX}:{posicaoY}, virado para o {stgApontando}");
+                Console.ReadLine();
+                
             }
-
-            string stgApontando = null;
-            string [] pontosCardiais = {null, "norte", "leste", "sul", "oeste"};
-
-            for (int i = 1; i < 5; i++)
-            {
-                if (apontando == i)
-                {
-                    stgApontando = pontosCardiais[i];
-                }
-            }
-
-            Console.WriteLine($"O robô está na localização {posicaoX}:{posicaoY}, virado para o {stgApontando}");
-            Console.ReadLine();
         }
     }
 }
